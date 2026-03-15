c578da4890ffa0068a0dc325f5dc01e8fb5fa5a1
Adarsh Dwivedi <adarshdwivedi619@gmail.com>
2026-03-15 12:56:45 +0530

    Fix Java version to 21 and add CORS

--- loveB/src/main/java/com/AD/loveB/LoveBApplication.java
+++ loveB/src/main/java/com/AD/loveB/LoveBApplication.java
@@ -2,13 +2,28 @@ package com.AD.loveB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LoveBApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(LoveBApplication.class, args);
-		System.out.println("heyy");
+		System.out.println("Backend is running on Render!");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// This allows your React frontend to talk to this backend
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 }
